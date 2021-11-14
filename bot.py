e79a6972c8056aa5643bec3052f04f000af6b315
Frost <60550481+TechnoFrost27@users.noreply.github.com>
2021-11-14 15:26:22 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,35 +1,16 @@
-from giphy_client.rest import ApiException
-import giphy_client
-import aiohttp
-import sys
-from discord_components import DiscordComponents, Button, ButtonStyle, Select, SelectOption
-from discord_components import *
 import asyncio
-import time
-import discord
-import urbandict
-from discord.utils import get
-from discord.ext import commands, tasks
-import random
+import nextcord
+from nextcord.ext import commands, tasks
 import os
-import requests
-import re
-import datetime
-from PIL import Image, ImageFont, ImageDraw
-import math
-from io import BytesIO
 from tinydb import TinyDB, Query
-from discord.ext.commands import MissingPermissions
-from pprint import pprint
-import typing
-from discord.ext.commands import cooldown, BucketType
 from dotenv import load_dotenv
-import topgg
-
-
 load_dotenv()
-intents = discord.Intents.default()
+from discord_slash import SlashContext, SlashCommand
+from discord_slash import cog_ext
+
+intents = nextcord.Intents.default()
 intents.members = True
+
 
 async def determine_prefix(bot, message):
     if message.guild:
@@ -47,8 +28,11 @@ async def determine_prefix(bot, message):
         return '!'
 
 
-bot = commands.Bot(command_prefix=determine_prefix, intents=intents)
+bot = commands.Bot(command_prefix=determine_prefix, intents=intents, enable_debug_events=True)
+slash = SlashCommand(bot, sync_commands=True)
+
 bot.remove_command('help')
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         try:
@@ -58,8 +42,8 @@ for filename in os.listdir('./cogs'):
             print(f'{e.__class__.__name__}: {e}')
             
 bot.load_extension("uptime")
-dbl_token = os.getenv(dbl_token)
-bot.topggpy = topgg.DBLClient(bot, dbl_token, autopost=True)
+
+
 
 @bot.event
 async def on_message(message):
@@ -76,31 +60,9 @@ async def on_message(message):
             return
     except:
         print("It's a DM")
-
-    db2 = TinyDB('databases/afk.json')
-    query = Query()
-
-    for member in message.mentions:
-        if db2.search(query['afk_user'] == member.id):
-            value = str(
-                list(
-                    map(lambda entry: entry["reason"],
-                        db2.search(query['afk_user'] == member.id)))[0])
-            await message.channel.send(
-                embed=discord.Embed(title=f"{member.display_name} is currently afk",
-                                    description=f"Afk note is: {value}",
-                                    color=discord.Color.random()))
-
-    member = message.author
-    if db2.search(query['afk_user'] == member.id):
-        await message.channel.send(embed=discord.Embed(
-            title=f"{member.display_name} You typed a message!",
-            description=f"That means you ain't afk!\nWelcome back buddy.",
-            color=discord.Color.random()))
-
-        query = Query()
-        db2.remove(query.afk_user == member.id)
+    # executed print be4 it
     await bot.process_commands(message=message)
+    # executed print after it
 
 
 @bot.command()
@@ -116,14 +78,14 @@ async def load(ctx, *, module):
         try:
             bot.load_extension(f"cogs.{module}")
         except commands.ExtensionError as e:
-            await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
+            await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
         else:
-            embed1 = discord.Embed(title=f"Alright {author}. Loaded {module}.py with no errors",
-                                   description=f"<a:zo_thumbs_up:886219697694081045>", color=discord.Color.random())
+            embed1 = nextcord.Embed(title=f"Alright {author}. Loaded {module}.py with no errors",
+                                    description=f"<a:zo_thumbs_up:886219697694081045>", color=nextcord.Color.random())
             await ctx.send(embed=embed1)
 
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
 
 
 @bot.command()
@@ -139,14 +101,14 @@ async def unload(ctx, *, module):
         try:
             bot.unload_extension(f"cogs.{module}")
         except commands.ExtensionError as e:
-            await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
+            await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
         else:
-            embed1 = discord.Embed(title=f"Alright {author}. Unloaded {module}.py with no errors",
-                                   description=f"<a:zo_thumbs_up:886219697694081045>", color=discord.Color.random())
+            embed1 = nextcord.Embed(title=f"Alright {author}. Unloaded {module}.py with no errors",
+                                    description=f"<a:zo_thumbs_up:886219697694081045>", color=nextcord.Color.random())
             await ctx.send(embed=embed1)
 
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
 
 
 @bot.command()
@@ -162,24 +124,19 @@ async def reload(ctx, *, module):
         try:
             bot.reload_extension(f"cogs.{module}")
         except commands.ExtensionError as e:
-            await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error<a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
+            await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error<a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
         else:
-            embed1 = discord.Embed(title=f"Alright {author}. Reloaded {module}.py with no errors",
-                                   description=f"<a:zo_thumbs_up:886219697694081045>", color=discord.Color.random())
+            embed1 = nextcord.Embed(title=f"Alright {author}. Reloaded {module}.py with no errors",
+                                   description=f"<a:zo_thumbs_up:886219697694081045>", color=nextcord.Color.random())
             await ctx.send(embed=embed1)
 
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
 
 
 @bot.command(aliases=["reloadall", "reloadcogs"])
 async def massreload(ctx):
     if ctx.author.id == 815555652780294175 or ctx.author.id == 723032217504186389:
-        if ctx.author.id == 815555652780294175:
-            author = "Mr One"
-
-        elif ctx.author.id == 723032217504186389:
-            author = "Mr Zero"
 
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
@@ -190,10 +147,11 @@ async def massreload(ctx):
                     await ctx.send(f"Done Reloading {filename[:-3]}, now moving on to the next one")
 
                 except commands.ExtensionError as e:
-                    await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
-            
+                    await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
+
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
+
 
 @bot.command(aliases=["unloadall", "unloadcogs"])
 async def massunload(ctx):
@@ -203,9 +161,9 @@ async def massunload(ctx):
 
         elif ctx.author.id == 723032217504186389:
             author = "Mr Zero"
-            
+
         for filename in os.listdir('./cogs'):
-            if filename.endswith('.py'): 
+            if filename.endswith('.py'):
                 await ctx.send(f"Unloading {filename[:-3]}")
                 await asyncio.sleep(1)
                 try:
@@ -213,7 +171,8 @@ async def massunload(ctx):
                     await ctx.send(f"Done Unloading {filename[:-3]}, now moving on to the next one")
 
                 except commands.ExtensionError as e:
-                    await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error<a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
+                    await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error<a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
+
 
 @bot.command(aliases=["loadall", "loadcogs"])
 async def massload(ctx):
@@ -223,7 +182,7 @@ async def massload(ctx):
 
         elif ctx.author.id == 723032217504186389:
             author = "Mr Zero"
-            
+
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 await ctx.send(f"Loading {filename[:-3]}")
@@ -233,13 +192,13 @@ async def massload(ctx):
                     await ctx.send(f"Done Loading {filename[:-3]}, now moving on to the next one")
 
                 except commands.ExtensionError as e:
-                    await ctx.send(embed=discord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=discord.Color.random()))
+                    await ctx.send(embed=nextcord.Embed(title="Oof Buddy, there is an error <a:zo_cri:886222278331867187>", description=f'{e.__class__.__name__}: {e}', color=nextcord.Color.random()))
 
 
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
 
-    
+
 @bot.command(aliases=["checkcogs"])
 async def checkcog(ctx):
     if ctx.author.id == 815555652780294175 or ctx.author.id == 723032217504186389:
@@ -248,15 +207,15 @@ async def checkcog(ctx):
 
         elif ctx.author.id == 723032217504186389:
             author = "Mr Zero"
-        
-        all_cogs=[]
-        loaded_cogs=[]
+
+        all_cogs = []
+        loaded_cogs = []
         for filename in os.listdir('./cogs'):
 
             if filename.endswith('.py'):
                 print(filename[:-3])
                 all_cogs.append(filename[:-3])
-                
+
         await ctx.send(f"Hey {author} All cogs are [{', '.join(all_cogs)}]")
 
         for i in all_cogs:
@@ -270,13 +229,8 @@ async def checkcog(ctx):
 
         await ctx.send(f"Hey {author} All loaded cogs are [{', '.join(loaded_cogs)}]")
     else:
-        await ctx.send(embed=discord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=discord.Color.random()))
+        await ctx.send(embed=nextcord.Embed(title="Nope you imposter", description="I dont take orders from peasants like you <a:ZOWumpusTongue:865559251764903946>", color=nextcord.Color.random()))
 
-
-
-@bot.event
-async def on_autopost_success():
-    print(f"Posted server count ({bot.topggpy.guild_count}))")
       
 token = os.getenv("DISCORD_BOT_SECRET")
 bot.run(token)
--- uptime.py
+++ uptime.py
@@ -1,5 +1,5 @@
-import discord, datetime, time
-from discord.ext import commands
+import nextcord, datetime, time
+from nextcord.ext import commands
 
 start_time = time.time()
 
@@ -13,13 +13,13 @@ class Uptime(commands.Cog):
         current_time = time.time()
         difference = int(round(current_time - start_time))
         text = str(datetime.timedelta(seconds=difference))
-        embed = discord.Embed(colour=discord.Color.random())
+        embed = nextcord.Embed(colour=nextcord.Color.random())
         embed.add_field(name="Uptime", value=text)
         embed.set_footer(text="Chad")
         try:
            # await ctx.send(embed=embed)
-            await ctx.send(embed=discord.Embed(title=f"Chad has been online for:",description=f"{text}<a:zo_tick_anim:886924589546995803>",color=discord.Color.random()))
-        except discord.HTTPException:
+            await ctx.send(embed=nextcord.Embed(title=f"Chad has been online for:",description=f"{text}<a:zo_tick_anim:886924589546995803>",color=nextcord.Color.random()))
+        except nextcord.HTTPException:
             await ctx.send("Current uptime: " + text)
 
 
